68090509241a848ba575cfc8e0217aff9e6e7baf
Oittaa <eero@oittaa.net>
2021-01-19 10:41:08 +0100

    Serve test images from GitHub

--- test.py
+++ test.py
@@ -6,14 +6,15 @@ from main import app, sha256sum
 from unittest.mock import patch
 
 TEST_LOCAL_JPG = 'static/flowers.jpg'
-TEST_LOCAL_GIF = 'static/test.gif'
-TEST_NET_JPG = 'https://www.gstatic.com/webp/gallery/5.jpg'
-TEST_NET_JPG_HASH = 'e7273a6e8842280c3e291297f3c6e3f7d94bb4c0993d771341ec5e22532e6411'  # curl -s https://www.gstatic.com/webp/gallery/5.jpg | sha256sum | head -c 64
-TEST_NET_PNG = 'https://www.gstatic.com/webp/gallery3/2.png'
-TEST_NET_BMP = 'https://www.w3.org/People/mimasa/test/imgformat/img/w3c_home.bmp'
+TEST_LOCAL_JPG_HASH = '17aba341360280d48dc475adf0bc4dc90a623b31e652c35fa1e1b966e921b7dd'  # sha256sum static/flowers.jpg | head -c 64
+TEST_NET_URL = 'https://raw.githubusercontent.com/oittaa/avif-converter/master/test_images/'
+TEST_NET_GIF = TEST_NET_URL + 'test.gif'
+TEST_NET_JPG = TEST_NET_URL + 'test.jpg'
+TEST_NET_JPG_HASH = 'e7273a6e8842280c3e291297f3c6e3f7d94bb4c0993d771341ec5e22532e6411'  # sha256sum test_images/test.jpg | head -c 64
+TEST_NET_PNG = TEST_NET_URL + 'test.png'
+TEST_NET_BMP = TEST_NET_URL + 'test.bmp'
 TEST_NET_NOT_IMAGE = 'https://www.google.com/'
-TEST_NET_TOO_BIG = 'https://effigis.com/wp-content/uploads/2015/02/Airbus_Pleiades_50cm_8bit_RGB_Yogyakarta.jpg'
-
+TEST_NET_TOO_BIG = TEST_NET_URL + 'test_50mb.jpg'
 
 # https://docs.python.org/3/library/unittest.mock.html#quick-guide
 # Note: When you nest patch decorators the mocks are passed in to
@@ -53,14 +54,14 @@ class SmokeTests(unittest.TestCase):
         response = self.app.post('/api', data={'file': open(TEST_LOCAL_JPG, 'rb')})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.headers.get('Content-Type'), 'image/avif')
-        response = self.app.post('/api', data={'file': open(TEST_LOCAL_GIF, 'rb')})
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.headers.get('Content-Type'), 'image/avif')
 
     @patch('main.upload_blob', side_effect=exceptions.NotFound('Test'))
     @patch('main.download_blob', side_effect=exceptions.NotFound('Test'))
     def test_api_get(self, mock_dl, mock_ul):
         response = self.app.get('/api?url={}'.format(urllib.parse.quote(TEST_NET_PNG)))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.headers.get('Content-Type'), 'image/avif')
+        response = self.app.get('/api?url={}'.format(urllib.parse.quote(TEST_NET_GIF)))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.headers.get('Content-Type'), 'image/avif')
         mock_dl.side_effect = self.download_blob
@@ -95,6 +96,7 @@ class SmokeTests(unittest.TestCase):
     def test_sha256sum(self):
         val1 = sha256sum(TEST_LOCAL_JPG)
         self.assertEqual(len(val1), 64)
+        self.assertEqual(val1, TEST_LOCAL_JPG_HASH)
         val2 = sha256sum(__file__)
         self.assertNotEqual(val1, val2)
 
