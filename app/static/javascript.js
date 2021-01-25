a48bf194b634aa0be1e929705dfd9f4126b2421c
Oittaa <eero@oittaa.net>
2021-01-25 13:42:40 +0100

    Loading spinner

--- app/static/javascript.js
+++ app/static/javascript.js
@@ -1,22 +1,33 @@
 function giveOnchangeEvent(input) {
     document.getElementById('file').onchange = function() {
+        showSpinner();
         document.getElementById('form1').submit();
     }
 }
 function delaySubmit(input, delay = 50) {
     setTimeout(function() {
         if (document.getElementById('url').value.startsWith('http')) {
+            showSpinner();
             document.getElementById('form2').submit();
         }
     }, delay);
 }
-function toggleVisibility(input) {
+function toggleApiVisibility(input) {
     var e = document.getElementById('api-info');
     if (e.style.display == 'block')
         e.style.display = 'none';
     else
         e.style.display = 'block';
 }
+function showSpinner(){
+  document.getElementById("spinner-back").classList.add("show");
+  document.getElementById("spinner-front").classList.add("show");
+}
+function hideSpinner(){
+  document.getElementById("spinner-back").classList.remove("show");
+  document.getElementById("spinner-front").classList.remove("show");
+}
 document.getElementById('file').addEventListener('focus', giveOnchangeEvent);
 document.getElementById('url').addEventListener('paste', delaySubmit);
-document.getElementById('api').addEventListener('click', toggleVisibility);
+document.getElementById('api').addEventListener('click', toggleApiVisibility);
+document.getElementById('form2').addEventListener('submit', showSpinner);
