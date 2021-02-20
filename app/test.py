865852b1f1ca529cacd108c11b9c4219c15b7d65
oittaa <oittaa@users.noreply.github.com>
2021-02-20 03:21:52 +0200

    Cache class (#3)

--- app/test.py
+++ app/test.py
@@ -3,19 +3,20 @@ import subprocess
 import unittest
 import urllib
 
-from google.cloud import exceptions
 from main import app, calculate_sri_on_file, get_extension, sha256sum
 from tempfile import NamedTemporaryFile
 from unittest.mock import patch
 
 TEST_LOCAL_PNG = "static/tux.png"
-TEST_LOCAL_PNG_HASH = "4358b1e6137fd60a49ad90d108b73c0116738552d78cf4fceb56a89f044c342f"  # sha256sum static/tux.png | head -c 64
+# sha256sum static/tux.png | head -c 64
+TEST_LOCAL_PNG_HASH = "4358b1e6137fd60a49ad90d108b73c0116738552d78cf4fceb56a89f044c342f"
 TEST_NET_URL = (
     "https://raw.githubusercontent.com/oittaa/avif-converter/master/test_images/"
 )
 TEST_NET_GIF = TEST_NET_URL + "test.gif"
 TEST_NET_JPG = TEST_NET_URL + "test.jpg"
-TEST_NET_JPG_HASH = "e7273a6e8842280c3e291297f3c6e3f7d94bb4c0993d771341ec5e22532e6411"  # sha256sum test_images/test.jpg | head -c 64
+# sha256sum test_images/test.jpg | head -c 64
+TEST_NET_JPG_HASH = "e7273a6e8842280c3e291297f3c6e3f7d94bb4c0993d771341ec5e22532e6411"
 TEST_NET_PNG = TEST_NET_URL + "test.png"
 TEST_NET_BMP = TEST_NET_URL + "test.bmp"
 TEST_NET_PNG_NOEXT = TEST_NET_URL + "test_png"
@@ -24,9 +25,15 @@ TEST_NET_AVIF = TEST_NET_URL + "test.avif"
 TEST_NET_PDF = TEST_NET_URL + "test.pdf"
 TEST_NET_NOT_IMAGE = "https://www.google.com/"
 TEST_NET_TOO_BIG = TEST_NET_URL + "test_50mb.jpg"
-EMPTY_FILE_SRI = "sha384-OLBgp1GsljhM2TJ+sbHjaiH9txEUvgdDTAzHv2P24donTt6/529l+9Ua0vFImLlb"  # echo -n "" | openssl dgst -sha384 -binary | openssl base64 -A
+# echo -n "" | openssl dgst -sha384 -binary | openssl base64 -A
+EMPTY_FILE_SRI = (
+    "sha384-OLBgp1GsljhM2TJ+sbHjaiH9txEUvgdDTAzHv2P24donTt6/529l+9Ua0vFImLlb"
+)
 TEST_STRING = "alert('Hello, world.');"
-TEST_STRING_SRI = "sha384-H8BRh8j48O9oYatfu5AZzq6A9RINhZO5H16dQZngK7T62em8MUt1FLm52t+eX6xO"  # echo -n "alert('Hello, world.');" | openssl dgst -sha384 -binary | openssl base64 -A
+# echo -n "alert('Hello, world.');" | openssl dgst -sha384 -binary | openssl base64 -A
+TEST_STRING_SRI = (
+    "sha384-H8BRh8j48O9oYatfu5AZzq6A9RINhZO5H16dQZngK7T62em8MUt1FLm52t+eX6xO"
+)
 TEST_BASE_URL = "https://www.example.com/"
 
 
@@ -47,6 +54,25 @@ def is_avif(data):
             return False
 
 
+class Cache(object):
+    """Mock Cache"""
+
+    def __init__(self):
+        self.cache = {}
+
+    def get(self, key):
+        if key in self.cache:
+            return self.cache[key]
+        return None
+
+    def set(self, key, value):
+        self.cache[key] = value
+        return True
+
+    def has(self, key):
+        return key in self.cache
+
+
 # https://docs.python.org/3/library/unittest.mock.html#quick-guide
 # Note: When you nest patch decorators the mocks are passed in to
 # the decorated function in the same order they applied (the normal
@@ -58,25 +84,8 @@ def is_avif(data):
 @patch("main.URL", TEST_BASE_URL)
 class SmokeTests(unittest.TestCase):
     def setUp(self):
-        self.cache = {}
         self.app = app.test_client()
         self.assertEqual(app.debug, False)
-
-    def download_blob(self, bucket_name, source_blob_name, destination_file_name):
-        key = bucket_name + source_blob_name
-        if key not in self.cache:
-            raise exceptions.NotFound(key)
-        with open(destination_file_name, "wb") as f:
-            f.write(self.cache[key])
-
-    def upload_blob(self, bucket_name, source_file_name, destination_blob_name):
-        key = bucket_name + destination_blob_name
-        with open(source_file_name, "rb") as f:
-            self.cache[key] = f.read()
-
-    def blob_exists(self, bucket_name, blob_name):
-        key = bucket_name + blob_name
-        return key in self.cache
 
     def test_main_page(self):
         response = self.app.get("/")
@@ -89,73 +98,61 @@ class SmokeTests(unittest.TestCase):
         response = self.app.get("/?test")
         self.assertEqual(response.status_code, 404)
 
-    @patch("main.blob_exists")
-    @patch("main.upload_blob", side_effect=exceptions.NotFound("Test"))
-    @patch("main.download_blob", side_effect=exceptions.NotFound("Test"))
-    def test_api_post(self, mock_dl, mock_ul, mock_be):
-        mock_be.side_effect = self.blob_exists
+    def test_api_post(self):
         response = self.app.post("/api", data={"file": open(TEST_LOCAL_PNG, "rb")})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.headers.get("Content-Type"), "image/avif")
         self.assertTrue(is_avif(response.data))
-        mock_dl.side_effect = self.download_blob
-        mock_ul.side_effect = self.upload_blob
-        response = self.app.post(
-            "/api", data={"file": open(TEST_LOCAL_PNG, "rb")}, follow_redirects=True
-        )
+        response = self.app.post("/api", data={"file": open(TEST_LOCAL_PNG, "rb")})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.headers.get("Content-Type"), "image/avif")
         self.assertTrue(is_avif(response.data))
 
-    @patch("main.blob_exists")
-    @patch("main.upload_blob", side_effect=exceptions.NotFound("Test"))
-    @patch("main.download_blob", side_effect=exceptions.NotFound("Test"))
-    def test_api_get(self, mock_dl, mock_ul, mock_be):
-        mock_be.side_effect = self.blob_exists
-        response = self.app.get("/api?url={}".format(urllib.parse.quote(TEST_NET_PNG)))
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.headers.get("Content-Type"), "image/avif")
-        self.assertTrue(is_avif(response.data))
-        response = self.app.get("/api?url={}".format(urllib.parse.quote(TEST_NET_GIF)))
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.headers.get("Content-Type"), "image/avif")
-        self.assertTrue(is_avif(response.data))
+    @patch("main.cache", Cache())
+    def test_api_get(self):
         response = self.app.get(
-            "/api?url={}".format(urllib.parse.quote(TEST_NET_PNG_NOEXT, "rb"))
+            "/api?url={}".format(urllib.parse.quote(TEST_NET_PNG)),
+            follow_redirects=True,
         )
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.headers.get("Content-Type"), "image/avif")
         self.assertTrue(is_avif(response.data))
         response = self.app.get(
-            "/api?url={}".format(urllib.parse.quote(TEST_NET_PDF, "rb"))
+            "/api?url={}".format(urllib.parse.quote(TEST_NET_GIF)),
+            follow_redirects=True,
         )
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.headers.get("Content-Type"), "image/avif")
         self.assertTrue(is_avif(response.data))
         response = self.app.get(
-            "/api?url={}".format(urllib.parse.quote(TEST_NET_HEIC, "rb"))
+            "/api?url={}".format(urllib.parse.quote(TEST_NET_PNG_NOEXT, "rb")),
+            follow_redirects=True,
         )
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.headers.get("Content-Type"), "image/avif")
         self.assertTrue(is_avif(response.data))
         response = self.app.get(
-            "/api?url={}".format(urllib.parse.quote(TEST_NET_AVIF, "rb"))
+            "/api?url={}".format(urllib.parse.quote(TEST_NET_PDF, "rb")),
+            follow_redirects=True,
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.headers.get("Content-Type"), "image/avif")
+        self.assertTrue(is_avif(response.data))
+        response = self.app.get(
+            "/api?url={}".format(urllib.parse.quote(TEST_NET_HEIC, "rb")),
+            follow_redirects=True,
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.headers.get("Content-Type"), "image/avif")
+        self.assertTrue(is_avif(response.data))
+        response = self.app.get(
+            "/api?url={}".format(urllib.parse.quote(TEST_NET_AVIF, "rb")),
+            follow_redirects=True,
         )
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.headers.get("Content-Type"), "image/avif")
         r = requests.get(TEST_NET_AVIF)
         self.assertEqual(response.data, r.content)
-        mock_dl.side_effect = self.download_blob
-        self.cache["-testing-" + TEST_NET_JPG_HASH + ".avif"] = "FAKEDATA".encode(
-            "utf-8"
-        )
-        response = self.app.get(
-            "/api?url={}".format(urllib.parse.quote(TEST_NET_JPG)),
-            follow_redirects=True,
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, "FAKEDATA".encode("utf-8"))
-        mock_ul.side_effect = self.upload_blob
         response = self.app.get(
             "/api?url={}".format(urllib.parse.quote(TEST_NET_BMP)),
             follow_redirects=True,
@@ -171,11 +168,7 @@ class SmokeTests(unittest.TestCase):
         self.assertEqual(response.headers.get("Content-Type"), "image/avif")
         self.assertEqual(response.data, temp_data)
 
-    @patch("main.blob_exists")
-    @patch("main.upload_blob", side_effect=exceptions.NotFound("Test"))
-    @patch("main.download_blob", side_effect=exceptions.NotFound("Test"))
-    def test_api_invalid_requests(self, mock_dl, mock_ul, mock_be):
-        mock_be.side_effect = self.blob_exists
+    def test_api_invalid_requests(self):
         response = self.app.get(
             "/api?url={}".format(urllib.parse.quote(TEST_NET_NOT_IMAGE))
         )
