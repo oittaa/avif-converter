8b342f1c9ba602f4602249f9d8e19aeb6c2144e3
Oittaa <eero@oittaa.net>
2021-02-24 21:02:17 +0100

    ARM64

--- app/coverage.sh
+++ app/coverage.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 set -e
+ARCH=$(uname -m)
+echo ARCH $ARCH
+if [ "$ARCH" != "x86_64" ]
+then
+    apt-get update
+    apt-get install -y gcc build-essential python3-dev
+fi
+pip3 install --no-cache-dir -r requirements-dev.txt
+if [ "$ARCH" != "x86_64" ]
+then
+    apt-get remove --autoremove --purge -y gcc build-essential python3-dev
+    rm -rf /var/lib/apt/lists/*
+fi
 coverage run --source=./ --omit=test.py test.py
 coverage report -m
 cd /
